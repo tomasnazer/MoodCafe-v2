1c0de90315acb209e47548c5c412a220f2ecd60d
tomasnazer <104788616+tomasnazer@users.noreply.github.com>
2022-05-31 15:56:02 -0300

    about page updated

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section>
+        <picture class="logo-container">
+            <img src="../assets/images/logobiig.png" alt="mood-logo">
+        </picture>
+        <h2>Sobre Nosotros</h2>
+        <div class="text">
+            <p>Bienvenidos a Mood Café & Resto, un nuevo espacio para nuevos momentos , con una gran Gastronomía.</p>
+            <p>En esta cafetería vas a poder encontrar una gran variedad de opciones para disfrutar un buen desayuno o una rica merienda. </p>
+            <p>También vas a poder disfrutar de los productos de la franquicia balcarce</p>
+        </div>
+        <h2>Trabajá con nosotros</h2>
+        <div class="text">
+            <p>Si tenes experiencia siendo mozo, cafetero, personal de cocina</p>
+            <p>Envíanos un mail con tu curriculum a mail@mail.com</p>
+        </div>
+       
+    </section>
 </template>
+
+
+<style scoped>
+section{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 160px;
+}
+
+img{
+    padding: 0;
+    max-width: 100vw;
+}
+
+h2{
+    font-size: 2.2rem;
+    color: rgb(0, 0, 0);
+    font-weight: bolder;
+    margin-bottom: 5px;
+}
+
+.text{
+    font-size: 1.5rem;
+    margin: 20px 10px;
+    color: rgb(8, 8, 8);
+    font-weight: bold;
+}
+</style>
