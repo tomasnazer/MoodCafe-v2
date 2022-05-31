f80d73793c1fc95bd5d5a3d93d368273b2906c1c
tomasnazer <104788616+tomasnazer@users.noreply.github.com>
2022-05-31 15:59:45 -0300

    proxys created

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,32 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    proxy: {
+      '^/Coffe': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: { '^/Coffe': '/' },
+      },
+    },
+    proxy: {
+      '^/Drinks': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: { '^/Drinks': '/' },
+      },
+    },
+    proxy: {
+      '^/Food': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: { '^/Food': '/' },
+      },
+    },
+  },
+}
