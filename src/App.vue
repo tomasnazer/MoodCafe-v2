d15ec513787ec99cedda25c18e414af1b43267e0
tomasnazer <104788616+tomasnazer@users.noreply.github.com>
2022-05-31 15:44:52 -0300

    fixed bugs in app.vue

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav> -->
-  <Footer />
   <router-view/>
+  <Footer />
+  
 </template>
 
 <script>
