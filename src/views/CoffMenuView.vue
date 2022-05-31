0d2271d60652d9643fb12ee0be271458a6f24529
tomasnazer <104788616+tomasnazer@users.noreply.github.com>
2022-05-31 15:57:29 -0300

    coffe page created

---
+++ src/views/CoffMenuView.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <div class="Product-box" id="Product">  
+        </div>
+    </section>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  name: "Coffepage",
+  data(){
+      return {
+          products: null,
+      }
+  },
+  mounted(){
+      this.getProducts()
+  },
+  methods: {
+    async  getProducts () {
+        const res = await axios.get('http://localhost:5000/Coffe')
+        console.log(res)   
+    }
+  }
+}
+</script>
