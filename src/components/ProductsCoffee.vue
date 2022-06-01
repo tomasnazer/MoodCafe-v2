2f017cd926dd86ac6fbba6fb2abb854c6fe96c82
tomasnazer <104788616+tomasnazer@users.noreply.github.com>
2022-06-01 14:26:14 -0300

    adding styles

--- src/components/ProductsCoffee.vue
+++ src/components/ProductsCoffee.vue
@@ -15,3 +15,44 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+.Product-box {
+    display: flex;
+    flex-direction: column; 
+    align-items: center;
+    margin-bottom: 30px;
+}
+h1 {
+    margin-top: 20px;
+    margin-bottom: 5px;
+    font-size: 3rem;
+    font-weight: bold;
+    text-align: center;
+}
+.img-box {
+    border-radius: 50%;
+    margin-top: 10px;
+    margin-bottom: 50px;
+    box-shadow: rgba(17,12,46,0.15) 0px 48px 
+    100px 0px;
+}
+
+.product {
+    font-weight: bold;
+    font-size: 1.6rem;
+    background-color: rgb(255, 255, 255);
+    margin-bottom: 5px;
+}
+
+.description {
+    text-align: center;
+    margin-left: 9px;
+    margin-right: 9px;
+    color: black;
+    font-weight: bold;
+    margin-bottom: 5px;
+    
+}
+
+</style>
