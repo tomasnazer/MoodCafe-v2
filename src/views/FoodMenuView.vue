abc493ed22ea2e606c7cf65f2ab6bebb1d782c62
tomasnazer <104788616+tomasnazer@users.noreply.github.com>
2022-06-24 09:54:19 -0300

    search-bar added to foodmenu

--- src/views/FoodMenuView.vue
+++ src/views/FoodMenuView.vue
@@ -1,11 +1,25 @@
 <template>
-    <div  >
+    <div class="search-input">
+            <input type="text" 
+                    placeholder="Buscá tu producto..."
+                    name="uname" v-model="search" @focus="isInputOpen = !isInputOpen" @blur="isInputOpen= false"> 
+            <font-awesome-icon class="icono" icon="search" />
+    </div>
+    <div class="Product-box" v-for="product in filteredProducts" :key="product.id">
+            <h1 v-show="product.Titulo != ''">{{ product.Titulo }}</h1>
+            <img v-show="product.img != ''" :src="'/static/' + product.img" :alt="product.nombre" class="img-box">
+            <p class="product">{{product.nombre}} ${{product.precio}}</p>
+            <p class="description">{{product.descripcion}}</p>
+            <p class="stock" v-show="product.stock != 'true'">Sin stock!</p>
+    </div>
+    <div  v-if="isInputOpen === true">
         <FoodProducts :products="products" />
     </div>
 </template>
 
 <script>
 import FoodProducts from "../components/FoodProducts.vue"
+import { ref } from "vue";
 
 export default {
   name: "FoodPage",
@@ -15,10 +29,16 @@ export default {
 
   data(){
       return {
-          products: [],
-
+        search: '',
+        products: [],
+        isInputOpen: false,
       }
       
+  },
+  computed: {
+    filteredProducts() {
+        return this.products.filter(product => product.nombre.toLowerCase().includes(this.search.toLowerCase()))
+    }
   },
   mounted() {
           this.fetchProducts(),
@@ -47,6 +67,72 @@ export default {
 </script>
 
 <style scoped>
+input { 
+            width: 100%; 
+            padding: 12px 40px;  
+            display: inline-block; 
+            border: 1px solid #ccc; 
+            box-sizing: border-box; 
+            outline: none;
+} 
+.search-input {
+            position: relative;
+}
+          
+.icono {
+            position: absolute;
+            left: 10px;
+            top: 10px;
+            color: gray;
+            font-size: 20px;
+            text-align: center;
+}
+    
+.Product-box {
+    display: flex;
+    flex-direction: column; 
+    align-items: center;
+    margin-bottom: 30px;
+}
+h1 {
+    margin-top: 20px;
+    margin-bottom: 5px;
+    font-size: 3rem;
+    font-weight: bold;
+    text-align: center;
+}
+.img-box {
+    height: 300px;
+    width: 300px;
+    border-radius: 50%;
+    margin-top: 10px;
+    margin-bottom: 50px;
+    box-shadow: rgba(17,12,46,0.15) 0px 48px 
+    100px 0px;
+}
 
+.product {
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.6rem;
+    margin-bottom: 5px;
+}
+
+.description {
+    text-align: center;
+    margin-left: 9px;
+    margin-right: 9px;
+    color: black;
+    font-weight: bold;
+    margin-bottom: 5px;
+    
+}
+
+.stock {
+    color: white;
+    background: red;
+    font-size: 1.3rem;
+    font-weight: bold;
+}
 
 </style>
