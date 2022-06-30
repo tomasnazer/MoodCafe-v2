5701cd3499cf8060c6a264730817d5dece45732d
tomasnazer <104788616+tomasnazer@users.noreply.github.com>
2022-06-30 14:15:22 -0300

    updates

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
         <picture class="logo-container">
-            <img src="../assets/images/logobiig.png" alt="mood-logo">
+            <img src="../assets/images/local2.jpg" alt="mood-logo">
         </picture>
         <h2>Sobre Nosotros</h2>
         <div class="text">
@@ -9,6 +9,9 @@
             <p>En esta cafetería vas a poder encontrar una gran variedad de opciones para disfrutar un buen desayuno o una rica merienda. </p>
             <p>También vas a poder disfrutar de los productos de la franquicia balcarce</p>
         </div>
+        <picture class="logo-container">
+            <img src="../assets/images/local1.jpg" alt="mood-logo">
+        </picture>
         <h2>Trabajá con nosotros</h2>
         <div class="text">
             <p>Si tenes experiencia siendo mozo, cafetero, personal de cocina</p>
@@ -21,15 +24,21 @@
 
 <style scoped>
 section{
+    padding-top: 50px;
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-bottom: 190px;
+    margin-bottom: 120px;
 }
 
 img{
-    padding: 0;
-    max-width: 100vw;
+    height: 300px;
+    width: 300px;
+    border-radius: 50%;
+    margin-top: 10px;
+    margin-bottom: 50px;
+    box-shadow: rgba(17,12,46,0.15) 0px 48px 
+    100px 0px;
 }
 
 h2{
@@ -37,6 +46,7 @@ h2{
     color: rgb(0, 0, 0);
     font-weight: bolder;
     margin-bottom: 5px;
+    text-align: center;
 }
 
 .text{
@@ -44,5 +54,6 @@ h2{
     margin: 20px 10px;
     color: rgb(8, 8, 8);
     font-weight: bold;
+    text-align: center;
 }
 </style>
